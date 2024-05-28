0035ee775cdc8657d79cffea549fb6707bf42208
Alex Liu <lsj178@139.com>
2024-05-28 21:53:55 +0800

    add ball

--- play.py
+++ play.py
@@ -2,15 +2,29 @@ from omegaconf import OmegaConf
 import numpy as np
 from omegaconf import OmegaConf,DictConfig
 from sympy import symbols, sympify, lambdify
+from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp2d
 from matplotlib.colors import Normalize
 from matplotlib.cm import ScalarMappable
-
+# 初始化小球的位置
+ball_x = 0.5
+ball_y = 0.5
 def load_config(fname='app.yaml')->DictConfig:
     cfg = OmegaConf.load(fname)
     return cfg
 
-def show(title,xname,yname,X,Y,U,V):
+def show(cfg,p,x,y,X,Y,U,V):
+    global ball_x, ball_y
+    title,xname,yname=p.desc,p.X.name,p.Y.name
+    
+    # 创建插值函数
+    u_interp = interp2d(x, y, U*np.ones_like(V), kind='linear')
+    v_interp = interp2d(x, y, V, kind='linear')
+    ball_x,ball_y=x[0],y[-1]
+    step=(x[-1]-x[0])/cfg.cells_side/2
+    #print('ball pos:',ball_x,ball_y)
+
     # 计算矢量的大小
     magnitude = np.sqrt(U**2 + V**2)
 
@@ -20,41 +34,66 @@ def show(title,xname,yname,X,Y,U,V):
     # 创建颜色映射对象
     cmap = plt.cm.coolwarm
 
-    # 创建一个ScalarMappable对象，用于颜色映射和颜色条
+    # # 创建一个ScalarMappable对象，用于颜色映射和颜色条
     sm = ScalarMappable(norm=norm, cmap=cmap)
-
-    # 将归一化的大小映射到颜色映射中
     colors = [sm.to_rgba(m) for m in magnitude.ravel()]
 
     # 创建图形和轴
-    _, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(7, 6))
 
     # 绘制矢量场，为每个箭头指定颜色
+    #ax.quiver(X, Y, U, V)
     ax.quiver(X, Y, U/magnitude, V/magnitude, color=colors)
 
+    ball, = ax.plot(ball_x, ball_y, 'bo')  # 'bo' 表示蓝色圆点
+
+    def update(frame):
+        global ball_x, ball_y
+        # 使用插值函数获取当前位置的矢量分量
+        u = u_interp(ball_x, ball_y) #U[round((ball_x-x1)/(x2-x1)*N)]#
+        v = v_interp(ball_x, ball_y) #U[round((ball_y-y1)/(y2-y1)*N)]#
+        # 更新小球的位置
+        #dis=np.sqrt(u**2+v**2)
+        ball_x += u * step
+        ball_y += v * step
+        # 确保小球不会离开绘图区域
+        ball_x = np.clip(ball_x, min(x), max(x))
+        ball_y = np.clip(ball_y, min(x),  max(x))
+        ball.set_data(ball_x, ball_y)
+        return ball,
     # 显示图形
     plt.xlabel(xname)
     plt.ylabel(yname)
     plt.title(title)
+    ani = FuncAnimation(fig, update, frames=np.linspace(0, 1, cfg.animate_time*1000//16), blit=True,interval=16)
     plt.show()
 
-def main(cfg,pname='P1'):
+def main(cfg):
     N=cfg.cells_side
+    pname=cfg.show
     p=cfg.problems[pname]
     x1,x2=p.X.range
     y1,y2=p.Y.range
-    x = np.linspace(x1,x2, N)
-    y = np.linspace(y1,y2, N)
-    X, Y = np.meshgrid(x, y)
+    xs = np.linspace(x1,x2, N)
+    ys = np.linspace(y1,y2, N)
+    X, Y = np.meshgrid(xs, ys)
 
     x, y = symbols('X Y')
     u=sympify(p.U)
     v=sympify(p.V)
-    func1 = lambdify((x,y), u, 'numpy')
-    func2 = lambdify((x,y), v, 'numpy')
-    U=func1(X,Y)
-    V=func2(X,Y)
-    show(p.desc,p.X.name,p.Y.name,X,Y,U,V)
+    ks=[]
+    ds=[]
+    args=p.get('args',None)
+    if args!=None:
+        for arg in p.args.keys():
+            ks.append(symbols(arg))
+            ds.append(p.args[arg])
+
+    func1 = lambdify((x,y,*ks), u, 'numpy')
+    func2 = lambdify((x,y,*ks), v, 'numpy')
+    U=func1(X,Y,*ds)
+    V=func2(X,Y,*ds)
+    show(cfg,p,xs,ys,X,Y,U,V)
 
 # def check_config(cfg):
 #     print(cfg.cells_side)
@@ -84,4 +123,4 @@ def main(cfg,pname='P1'):
 #     print("计算结果：", result)    
 if __name__ == '__main__':
     cfg=load_config()
-    main(cfg,'P2')
+    main(cfg)
